872c8a90ce741335829d85a38e8adfe6d624d6d4
sndp bag <sndpbagg@gmail.com>
2025-02-21 08:23:03 +0530

    remove auth cre..

--- src/Auth/Firebase.config.js
+++ src/Auth/Firebase.config.js
@@ -5,14 +5,7 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD4rwqlqjIMKsCui8bQiuFSO0AfHgkscvE",
-//   authDomain: "singup-18412.firebaseapp.com",
-//   projectId: "singup-18412",
-//   storageBucket: "singup-18412.firebasestorage.app",
-//   messagingSenderId: "125722197742",
-//   appId: "1:125722197742:web:f80661b702f6ac0e65fdff"
-// };
+ 
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
