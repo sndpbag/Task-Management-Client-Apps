9c8355e4208741ac0105d9694ea8c8ace6c14c2a
sndp bag <sndpbagg@gmail.com>
2025-02-21 08:20:36 +0530

    make .env file and google auth api hidden

--- src/Auth/Firebase.config.js
+++ src/Auth/Firebase.config.js
@@ -5,13 +5,22 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD4rwqlqjIMKsCui8bQiuFSO0AfHgkscvE",
+//   authDomain: "singup-18412.firebaseapp.com",
+//   projectId: "singup-18412",
+//   storageBucket: "singup-18412.firebasestorage.app",
+//   messagingSenderId: "125722197742",
+//   appId: "1:125722197742:web:f80661b702f6ac0e65fdff"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD4rwqlqjIMKsCui8bQiuFSO0AfHgkscvE",
-  authDomain: "singup-18412.firebaseapp.com",
-  projectId: "singup-18412",
-  storageBucket: "singup-18412.firebasestorage.app",
-  messagingSenderId: "125722197742",
-  appId: "1:125722197742:web:f80661b702f6ac0e65fdff"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
